da2b70dc261ed27c47fd246e4d1cca4e340d1777
Lolowiec <m3c21mng2@gmail.com>
2023-07-05 19:46:36 +0200

    server and client

--- client.py
+++ client.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     ip = "127.0.0.1"
     port= 1234
 
-    server  = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-    server.connect((ip,port))
+    client  = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    client.connect((ip,port))
---
+++ server.py
@@ -0,0 +1,15 @@
+
+
+import socket   
+
+if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port= 1234
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen(5)
+    
+    while True:
+        client, address = server.accept()
+        print(f"Conection - {address[0]} : {address[1]} ") 
