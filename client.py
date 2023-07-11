a97c7158b1136f11012635579c3db074501d24f3
Lolowiec <m3c21mng2@gmail.com>
2023-07-11 13:17:05 +0200

    parsing HTTP

--- client.py
+++ client.py
@@ -1,14 +1,12 @@
-
 from http import server
-import socket    
+import socket
 
 if __name__ == "__main__":
     ip = "127.0.0.1"
-    port= 1234
+    port = 1235
 
-    client  = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-    client.connect((ip,port))
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((ip, port))
 
-    string = "GET / HTTP/1.1\nHost: localhost\n"
-    client.send(bytes(string, "utf-8"))
-
+    request = "GET / HTTP/1.1\nHost: GET localhost\n"
+    client.send(bytes(request, "utf-8"))
--- server.py
+++ server.py
@@ -1,19 +1,36 @@
-import socket  
+import socket
 
-if __name__ == "__main__":
-    ip = "127.0.0.1"
-    port= 1234
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip, port))
-    server.listen(5)
+def parse_HTTP(req):
+    return "GET"
 
-    while True:
-        client, address = server.accept()
-        print(f"Connection - {address[0]} : {address[1]} ") 
 
-        string = client.recv(1024)
-        string = string.decode("utf-8")
-        print(string)
+parse_HTTP("GET")
 
-        client.close()
+
+# if __name__ == "__main__":
+#     ip = "127.0.0.1"
+#     port= 1235
+
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server.bind((ip, port))
+#     server.listen(5)
+
+
+#     while True:
+#         client, address = server.accept()
+#         print(f"Connection - {address[0]} : {address[1]} ")
+
+#         request = client.recv(1024)
+#         request = request.decode("utf-8")
+#         if 'GET' in request :
+#             print('Its GET')
+#         if 'POST' in request :
+#             print('Its POST')
+#         if 'PUT' in request :
+#             print('Its PUT')
+#         if 'DELETE' in request :
+#             print('Its DELETE')
+
+#         print(request)
+#         client.close()
