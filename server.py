8f6b301e060a4c5e9e03c5d00573aee91d74473a
Lolowiec <m3c21mng2@gmail.com>
2023-07-26 11:52:40 +0200

    add Http req body

--- server.py
+++ server.py
@@ -1,11 +1,7 @@
 import unittest
 
 
-
-   
-
-
-class HTTPreq:
+class HttpRequest:
     def __init__(self, req: str): 
         reqlines = req.splitlines()                     #cały tekst 
         firstrow =  reqlines[0].split(' ')              #linia 0
@@ -13,46 +9,44 @@ class HTTPreq:
         self.method = firstrow[0]
         self.path = firstrow[1]
         self.verson = firstrow[2]
+        self.body = ''
+        if '\n\n' in req:
+            P = req.find('\n\n')
+            self.body = req[P+2:]
         for i in range(1, len(reqlines)) :
             headerrow = reqlines[i]                     #nagłówki
             B = headerrow.find(':')
             key = headerrow[:B]
             value = headerrow[B+1:]
             self.headers[key] = value.strip()
-            
-
- 
         #zmiena    vartość
 
-            
-        
-        
 
-
-class TestStringMethods(unittest.TestCase):
+class TestHttpRequest(unittest.TestCase):
     def test_minimalHTTPreq(self):                                      # METHOD                   PATH                 VERSON
-        reqGET = HTTPreq("GET /index.html HTTP/1.0")
+        reqGET = HttpRequest("GET /index.html HTTP/1.0")
         self.assertEqual(reqGET.method, "GET")
         self.assertEqual(reqGET.path, "/index.html")
         self.assertEqual(reqGET.verson, "HTTP/1.0")
 
-        reqPOST = HTTPreq("POST /index.html HTTP/1.0")
+        reqPOST = HttpRequest("POST /index.html HTTP/1.0")
         self.assertEqual(reqPOST.method, "POST")
         self.assertEqual(reqPOST.path, "/index.html")
         self.assertEqual(reqPOST.verson, "HTTP/1.0")
 
-        reqPUT = HTTPreq("PUT /index.html HTTP/1.0")
+        reqPUT = HttpRequest("PUT /index.html HTTP/1.0")
         self.assertEqual(reqPUT.method, "PUT")
         self.assertEqual(reqPUT.path, "/index.html")
         self.assertEqual(reqPUT.verson, "HTTP/1.0")
 
-        reqDELETE = HTTPreq("DELETE /index.html HTTP/1.0")
+        reqDELETE = HttpRequest("DELETE /index.html HTTP/1.0")
         self.assertEqual(reqDELETE.method, "DELETE")
         self.assertEqual(reqDELETE.path, "/index.html")
         self.assertEqual(reqDELETE.verson, "HTTP/1.0")
 
+
     def test_HTTPHeders(self):
-        req = HTTPreq(
+        req = HttpRequest(
             "GET /index.html HTTP/1.0\nHost:localhost:8000\nUser-Agent: Mozilla/5.0"
         )
         self.assertEqual(req.method, "GET")
@@ -62,7 +56,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(req.headers["User-Agent"], "Mozilla/5.0")
 
 
-        req = HTTPreq(
+        req = HttpRequest(
             "GET /index.html HTTP/1.0\nHost:localhost:8000 \nUser-Agent:  Mozilla/5.0 "
         )
         self.assertEqual(req.method, "GET")
@@ -70,7 +64,12 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(req.verson, "HTTP/1.0")
         self.assertEqual(req.headers["Host"], "localhost:8000")
         self.assertEqual(req.headers["User-Agent"], "Mozilla/5.0")
-        
+    
+    
+    def test_HTTPBody(self):
+        req = HttpRequest('GET /index.html HTTP/1.0\nContent-Length: 123\n\nHello World')
+        self.assertEqual(req.headers['Content-Length'], '123')
+        self.assertEqual(req.body,'Hello World')
 
 
 
